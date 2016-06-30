29f38cbfccab73ebb6e4dd1074d9826d776c3c46
Georgi Boychev <georgi.boychev@ontotext.com>
2016-06-30 17:06:10 +0300

    test for the whole PR

---
+++ access-point-atomizer/src/test/java/com/ontotext/ehri/tests/AtomizerTests.java
@@ -0,0 +1,45 @@
+package com.ontotext.ehri.tests;
+
+import com.ontotext.ehri.AccessPointAtomizerPR;
+import gate.Document;
+import gate.Factory;
+import gate.Gate;
+import gate.util.GateException;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+public class AtomizerTests {
+    private static final String DOCUMENT_PATH = "src/test/resources/random-sample.txt";
+    private static final String DOCUMENT_ENCODING = "UTF-8";
+
+    @Test
+    public void test() {
+
+        try {
+            URL documentURL = new File(DOCUMENT_PATH).toURI().toURL();
+
+            // initialize GATE
+            Gate.runInSandbox(true);
+            Gate.init();
+
+            // process document
+            Document document = Factory.newDocument(documentURL, DOCUMENT_ENCODING);
+            AccessPointAtomizerPR atomizer = new AccessPointAtomizerPR();
+            atomizer.setDocument(document);
+            atomizer.execute();
+
+            // check annotation counts
+            assertEquals("unexpected number of access-point annotations", 666, document.getAnnotations().get("Access Point").size());
+            assertEquals("unexpected number of atom annotations", 1229, document.getAnnotations().get("Atom").size());
+            assertEquals("unexpected number of token annotations", 1969, document.getAnnotations().get("Token").size());
+
+        } catch (MalformedURLException | GateException e) {
+            e.printStackTrace();
+        }
+    }
+}
