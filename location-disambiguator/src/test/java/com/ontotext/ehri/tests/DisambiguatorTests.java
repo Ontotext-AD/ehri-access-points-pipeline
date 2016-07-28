26918cb0634538494af5042d1ca66fb9dc8b9682
Georgi Boychev <georgi.boychev@ontotext.com>
2016-07-28 13:19:42 +0300

    more thorough test of the disambiguation function

--- location-disambiguator/src/test/java/com/ontotext/ehri/tests/DisambiguatorTests.java
+++ location-disambiguator/src/test/java/com/ontotext/ehri/tests/DisambiguatorTests.java
@@ -33,24 +33,53 @@ public class DisambiguatorTests {
         locations.iterator().next().add(new Location(4, "A.ADM", 0.0, 0.0, 0, 1, new HashSet<Integer>()));
         assertTrue("expected candidate with more links", disambiguate(locations, false).iterator().next().getId() == 4);
 
-        // test ancestor chaining
+        // test basic ancestor chaining without disambiguation ancestors
         locations = new HashSet<SortedSet<Location>>();
         SortedSet<Location> lookupOne = new TreeSet<Location>();
         lookupOne.add(new Location(1, "P.PPL", 0.0, 0.0, 0, 0, new HashSet<Integer>(Arrays.asList(4))));
         lookupOne.add(new Location(2, "P.PPL", 0.0, 0.0, 0, 0, new HashSet<Integer>()));
         SortedSet<Location> lookupTwo = new TreeSet<Location>();
         lookupTwo.add(new Location(3, "P.PPL", 0.0, 0.0, 0, 0, new HashSet<Integer>()));
-        lookupTwo.add(new Location(4, "A.ADM", 0.0, 0.0, 0, 1, new HashSet<Integer>()));
+        lookupTwo.add(new Location(4, "P.PPL", 0.0, 0.0, 0, 1, new HashSet<Integer>()));
         locations.add(lookupOne);
         locations.add(lookupTwo);
         disambiguated = disambiguate(locations, false);
         assertTrue("expected one result", disambiguated.size() == 1);
         assertTrue("expected candidate with ancestor", disambiguated.iterator().next().getId() == 1);
 
+        // test basic ancestor chaining with disambiguation ancestors
         disambiguated = disambiguate(locations, true);
         assertTrue("expected two results", disambiguated.size() == 2);
         Iterator<Location> iterator = disambiguated.iterator();
         assertTrue("expected candidate with more links first", iterator.next().getId() == 4);
         assertTrue("expected candidate with less links second", iterator.next().getId() == 1);
+
+        // test advanced ancestor chaining without disambiguation ancestors
+        locations = new HashSet<SortedSet<Location>>();
+        lookupOne = new TreeSet<Location>();
+        lookupOne.add(new Location(1, "P.PPL", 0.0, 0.0, 0, 2, new HashSet<Integer>(Arrays.asList(4))));
+        lookupOne.add(new Location(2, "P.PPL", 0.0, 0.0, 0, 0, new HashSet<Integer>()));
+        lookupTwo = new TreeSet<Location>();
+        lookupTwo.add(new Location(3, "P.PPL", 0.0, 0.0, 0, 1, new HashSet<Integer>(Arrays.asList(2))));
+        lookupTwo.add(new Location(4, "P.PPL", 0.0, 0.0, 0, 0, new HashSet<Integer>()));
+        SortedSet<Location> lookupThree = new TreeSet<Location>();
+        lookupThree.add(new Location(5, "P.PPL", 0.0, 0.0, 0, 0, new HashSet<Integer>()));
+        lookupThree.add(new Location(6, "P.PPL", 0.0, 0.0, 0, 1, new HashSet<Integer>()));
+        locations.add(lookupOne);
+        locations.add(lookupTwo);
+        locations.add(lookupThree);
+        disambiguated = disambiguate(locations, false);
+        assertTrue("expected two results", disambiguated.size() == 2);
+        iterator = disambiguated.iterator();
+        assertTrue("expected candidate with ancestor first", iterator.next().getId() == 1);
+        assertTrue("expected candidate with more links second", iterator.next().getId() == 6);
+
+        // test advanced ancestor chaining with disambiguation ancestors
+        disambiguated = disambiguate(locations, true);
+        assertTrue("expected three results", disambiguated.size() == 3);
+        iterator = disambiguated.iterator();
+        assertTrue("expected candidate with ancestor first", iterator.next().getId() == 1);
+        assertTrue("expected candidate with more links second", iterator.next().getId() == 6);
+        assertTrue("expected disambiguating ancestor third", iterator.next().getId() == 4);
     }
 }
