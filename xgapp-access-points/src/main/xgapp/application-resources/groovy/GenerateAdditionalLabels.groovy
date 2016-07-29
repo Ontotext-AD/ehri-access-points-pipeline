20b714708f219dd246aed3772c0eba4e0cf5d2c1
Georgi Boychev <georgi.boychev@ontotext.com>
2016-07-29 15:39:19 +0300

    groovy rule to generate additional labels for the ld gaz

---
+++ xgapp-access-points/src/main/xgapp/application-resources/groovy/GenerateAdditionalLabels.groovy
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern
+
+public class GenerateAdditionalLabels {
+	private static final Pattern STUFF_TO_REMOVE = Pattern.compile("^(bezirk|kreis|kz) |( (concentration camp)|(ska(ya)?)? oblast)$")
+
+	public static Set<String> workFlow(String funk, String input, String clazz) {
+		Set<String> result = new HashSet<String>()
+		result.add(input.replace("ae", "a").replace("oe", "o").replace("ue", "u"))
+		result.add(STUFF_TO_REMOVE.matcher(input).replaceAll(""))
+		return result
+	}
+}
